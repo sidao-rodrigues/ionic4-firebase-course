38bf59fd06b0a4a9206b518af149131f43a3776e
Sidão <sidaoswat@hotmail.com>
2020-05-16 12:43:08 -0300

    criando metodos de signIN e signUp

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private aAuth: AngularFireAuth) { }
+
+  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+    return this.aAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential> { /*Isto { email, password, name } equivale a como se fosse um model, mas poderia criara um*/
+    return this.aAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(credentials => 
+        credentials.user.updateProfile({ displayName: name, photoURL: null })  
+          .then(() => credentials)
+      );
+
+  }
 }
