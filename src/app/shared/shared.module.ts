19b8f94b2f76ad88a259539bccfdfc5e291c5a9f
Sidão <sidaoswat@hotmail.com>
2020-05-15 22:45:15 -0300

    refatorando importacoes e criando modulo responsavel compartilhado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-
-
 @NgModule({
   exports: [CommonModule, ReactiveFormsModule, IonicModule]
 })
 export class SharedModule { }
+/*Esses cara estão sendo usados nos componentes criados*/
