724dcdc647643ff8c1f58afcea25303db58bf0ac
Sidão <sidaoswat@hotmail.com>
2020-05-16 16:27:07 -0300

    criando decoradores @input e @output de task-item

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss'],
 })
-export class TaskItemComponent implements OnInit {
+export class TaskItemComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
+  @Input() public task: Task;
+  @Output() public done = new EventEmitter<Task>();
+  @Output() public update = new EventEmitter<Task>();
+  @Output() public delete = new EventEmitter<Task>();
 
 }
