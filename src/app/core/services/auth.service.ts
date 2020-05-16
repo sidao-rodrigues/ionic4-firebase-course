99dc81ac071cb618c7b52f8982189d4c5ef3dbe7
Sidão <sidaoswat@hotmail.com>
2020-05-16 12:50:35 -0300

    criando tipos de dados

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase';
+import { User, AuthProvider } from './auth.types';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,27 @@ export class AuthService {
 
   constructor(private aAuth: AngularFireAuth) { }
 
-  private signInWithEmail({ email, password }): Promise<auth.UserCredential> {
+  private signInWithEmail({ email, password }: User): Promise<auth.UserCredential> {
     return this.aAuth.auth.signInWithEmailAndPassword(email, password);
   }
 
-  private signUpWithEmail({ email, password, name }): Promise<auth.UserCredential> { /*Isto { email, password, name } equivale a como se fosse um model, mas poderia criara um*/
+  private signUpWithEmail({ email, password, name }: User): Promise<auth.UserCredential> { /*Isto { email, password, name } equivale a como se fosse um model, mas poderia criara um*/
     return this.aAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then(credentials => 
         credentials.user.updateProfile({ displayName: name, photoURL: null })  
           .then(() => credentials)
       );
+  }
 
+  private signInWithPopup(provider: AuthProvider): Promise<auth.UserCredential> {
+    let signInProvider = null;
+
+    switch(provider) {
+      case AuthProvider.Facebook:
+        signInProvider = new auth.FacebookAuthProvider();
+        break;
+    }
+    return this.aAuth.auth.signInWithPopup(signInProvider);
   }
 }
