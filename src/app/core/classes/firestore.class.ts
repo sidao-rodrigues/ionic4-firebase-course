3d935f41ac04110e5477d19a5d4ff5ae98efe74e
Sidão <sidaoswat@hotmail.com>
2020-05-16 17:53:01 -0300

    criando metodos de collections

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,3 +1,18 @@
+import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 export abstract class Firestore<T> {
+
+  protected collection: AngularFirestoreCollection<T>;
+
+  constructor(protected db: AngularFirestore) { }
+
+  protected setCollectio(path: string, queryFn?: QueryFn): void {
+    this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  public getAll(): Observable<T[]> {
+    return this.collection.valueChanges();
+  }
 
 }
