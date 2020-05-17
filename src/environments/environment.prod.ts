a6b192584b04f4e23ced41f1abb4745f812f07a0
Sidão <sidaoswat@hotmail.com>
2020-05-16 21:56:33 -0300

    curso finalizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDeQZu5gU_bhrXWGfRAU7wrr-NgL_Mc1bA",
+    authDomain: "ionic-4-tasks-with-firebase.firebaseapp.com",
+    databaseURL: "https://ionic-4-tasks-with-firebase.firebaseio.com",
+    projectId: "ionic-4-tasks-with-firebase",
+    storageBucket: "ionic-4-tasks-with-firebase.appspot.com",
+    messagingSenderId: "431470460336",
+    appId: "1:431470460336:web:f3c9cf4b8d825ec5812a75",
+    measurementId: "G-H6GXY7K93S"
+  }
 };
