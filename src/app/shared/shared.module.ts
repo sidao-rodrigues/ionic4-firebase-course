62940dd6fd44582876c56089bcd4b7d928b14a94
Sidão <sidaoswat@hotmail.com>
2020-05-16 21:19:39 -0300

    adicionando menu logout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+import { LogoutButtonComponent } from '../shared/components/logout-button/logout-button.component';
 
 @NgModule({
-  declarations: [MenuToggleComponent],
+  declarations: [LogoutButtonComponent, MenuToggleComponent],
   imports:[IonicModule],
-  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent]
+  exports: [CommonModule, ReactiveFormsModule, IonicModule, MenuToggleComponent, LogoutButtonComponent ]
 })
 export class SharedModule { }
 /*Esses cara estão sendo usados nos componentes criados*/
