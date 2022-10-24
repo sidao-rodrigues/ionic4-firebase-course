335b58e82169e3d25743f76a0e01f5581626d62c
Sidney Rodrigues <sidaoswat@hotmail.com>
2022-10-24 09:53:43 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDeQZu5gU_bhrXWGfRAU7wrr-NgL_Mc1bA",
-    authDomain: "ionic-4-tasks-with-firebase.firebaseapp.com",
-    databaseURL: "https://ionic-4-tasks-with-firebase.firebaseio.com",
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
     projectId: "ionic-4-tasks-with-firebase",
-    storageBucket: "ionic-4-tasks-with-firebase.appspot.com",
-    messagingSenderId: "431470460336",
-    appId: "1:431470460336:web:f3c9cf4b8d825ec5812a75",
-    measurementId: "G-H6GXY7K93S"
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   }
 };
